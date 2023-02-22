a7ee0c77b3b4b3d95626d68d367bc29d54469475
Art Zwinger <artzwinger@gmail.com>
2023-02-22 03:54:48 +0300

    add logging to publish articles

--- app/Http/Controllers/PublishArticles.php
+++ app/Http/Controllers/PublishArticles.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Carbon\Carbon;
+use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class PublishArticles extends Controller
 {
@@ -15,7 +18,23 @@ class PublishArticles extends Controller
      */
     public function __invoke(Request $request): JsonResponse
     {
+        DB::listen(function (QueryExecuted $event) {
+            Log::info(
+                'SQL Query',
+                [
+                    $event->sql,
+                    $event->bindings,
+                    $event->time,
+                ]
+            );
+        });
         $ids = $request->post('articles_ids');
+        Log::info(
+            'Publish articles',
+            [
+                'ids' => json_encode($ids)
+            ]
+        );
         $articles = Article::whereId($ids)->get(['id']);
         Article::query()->whereIn('id', $ids)->update(['updated' => false, 'published_at' => Carbon::now()]);
         $updatedArticlesIds = $articles->map(fn($article) => (string)$article->id);
